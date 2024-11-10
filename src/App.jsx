0fe83bd4e51cf8152d0fdbcc6dcbfd9f3b5127c2
lalit <lalit.pratap.naidu@gmail.com>
2024-11-10 14:51:38 -0500

    setup front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Menubar } from "primereact/menubar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "Blog",
+      icon: "pi pi-star",
+    },
+    {
+      label: "Company",
+      icon: "pi pi-search",
+      items: [
+        {
+          label: "Background",
+          icon: "pi pi-bolt",
+        },
+        {
+          label: "About Us",
+          icon: "pi pi-server",
+        },
+        {
+          label: "Mission",
+          icon: "pi pi-pencil",
+        },
+      ],
+    },
+    {
+      label: "Contact",
+      icon: "pi pi-envelope",
+    },
+  ];
+
+  const onClick = (e) => {
+    console.log("click ");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Menubar model={items} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
