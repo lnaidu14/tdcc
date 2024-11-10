4d07449bdefb3950354eda77cd220fd67af8a466
lalit <lalit.pratap.naidu@gmail.com>
2024-11-10 16:35:49 -0500

    add nav to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 import Home from "./routes/Home";
 import Contact from "./routes/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <Contact />,
-  },
-  {
-    path: "/background",
-    element: <Contact />,
-  },
-  {
-    path: "/mission",
-    element: <Contact />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    element: <Navigation />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <Contact />,
+      },
+      {
+        path: "/background",
+        element: <Contact />,
+      },
+      {
+        path: "/mission",
+        element: <Contact />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,62 @@
+import { Menubar } from "primereact/menubar";
+import { useNavigate } from "react-router-dom";
+
+export default function Navigation() {
+  const navigate = useNavigate();
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+      command: () => {
+        navigate("/");
+      },
+    },
+    {
+      label: "Blog",
+      icon: "pi pi-star",
+      command: () => {
+        navigate("/contact");
+      },
+    },
+    {
+      label: "Company",
+      icon: "pi pi-search",
+      items: [
+        {
+          label: "Background",
+          icon: "pi pi-bolt",
+          command: () => {
+            navigate("/contact");
+          },
+        },
+        {
+          label: "About Us",
+          icon: "pi pi-server",
+          command: () => {
+            navigate("/contact");
+          },
+        },
+        {
+          label: "Mission",
+          icon: "pi pi-pencil",
+          command: () => {
+            navigate("/contact");
+          },
+        },
+      ],
+    },
+    {
+      label: "Contact",
+      icon: "pi pi-envelope",
+      command: () => {
+        navigate("/contact");
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Menubar model={items} />
+    </>
+  );
+}
